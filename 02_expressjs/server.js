7996113db769202d822fc006fd4b71979e1e295a
ahmed dhafer <ahmedalkurdi485@gmail.com>
2026-04-16 04:41:17 +0300

    new

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express, { response, Router } from "express";
 
 const app = express();
 const port = 3000;
@@ -13,9 +13,10 @@ const cars = [
 
 const router = express.Router();
 
-app.get("/", (req, res) => {
-  res.send("hello from api");
+router.get("/", (req, res) => {
+  res.json(cars);
 });
+
 router.get("/:id", (req, res) => {
   const id = Number(req.params.id);
   const car = cars.find((car) => car.id === id);
@@ -25,33 +26,47 @@ router.get("/:id", (req, res) => {
   res.json(car);
 });
 
-router.get("/", (req, res) => {
-  res.json(cars);
-});
-
 router.post("/", (req, res) => {
   const { make, year, price } = req.body;
   if (!make || !year || !price) {
-    return res.status(400).json({Error:"missing"});
+    return res.status(400).json({ Error: "missing" });
   }
 
   const newCar = {
-    id: cars.length + 1 ,
-     make: make ,
-     year: Number(year) ,
-     price: Number(price) ,
-};
+    id: cars.length + 1,
+    make: make,
+    year: Number(year),
+    price: Number(price),
+  };
 
   cars.push(newCar);
   res.status(201).json(newCar);
 });
 
 router.put("/:id", (req, res) => {
-  res.send("update car");
+  const id = Number(req.params.id);
+  const index = cars.findIndex((c) => c.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "car not found" });
+  }
+  const { make, model, year, price } = req.body;
+
+  if (make) cars[index].make = make;
+  if (model) cars[index].model = model;
+  if (year) cars[index].year = Number(year);
+  if (price) cars[index].price = Number(price);
+
+  res.json(cars[index]);
 });
 
 router.delete("/:id", (req, res) => {
-  res.send("delete car");
+  const id = Number(req.params.id);
+  const index = cars.findIndex((c) => c.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "car not found" });
+  }
+  const deleted = cars.splice(index, 1)[0];
 });
 
 app.use("/api/v1/cars", router);
